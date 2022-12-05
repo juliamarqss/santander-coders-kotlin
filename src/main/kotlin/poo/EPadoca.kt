da980c8b594d509a296b5e95401d90437b2df868
Julia <juliamarques61@gmail.com>
2022-12-05 19:11:08 -0300

    feat: iniciando a refatoração do E-Padoca

---
+++ src/main/kotlin/poo/EPadoca.kt
@@ -0,0 +1,15 @@
+package poo
+
+private const val FINALIZAR = 0
+private const val PAES = 1
+private const val SALGADOS = 2
+private const val DOCES = 3
+private const val LINHA = "........."
+
+fun main() {
+
+}
+
+
+class EPadoca {
+}

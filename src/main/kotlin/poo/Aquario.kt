77ae52a6b24a6a4e6dcb5db62efd3c222dfefaff
Julia <juliamarques61@gmail.com>
2022-12-11 19:34:20 -0300

    feat: iniciando o desafio do Aquario

---
+++ src/main/kotlin/poo/Aquario.kt
@@ -0,0 +1,24 @@
+package poo
+
+private const val LINHA = "........."
+
+val menuInicial = """
+    
+    Digite a ação desejada no Menu:
+    
+    1${LINHA}Adicionar item
+    2${LINHA}Editar item
+    3${LINHA}Exibir itens em estoque
+    4${LINHA}Exibir todos os itens
+    0${LINHA}Fechar sistema
+""".trimIndent()
+
+
+fun main() {
+
+}
+
+class Aquario {
+
+}
+
